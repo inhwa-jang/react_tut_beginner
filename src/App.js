bb491148137e41b4c38aa18a0fa40646000151be
inhwa-jang <inhwa.jang01@gmail.com>
2022-10-04 01:16:27 +0900

    Router & useParams

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      {/* Switch : 한번에 하나의 Route만 렌더링 */}
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
